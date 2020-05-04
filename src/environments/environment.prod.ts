47de1930e17d6baaa2e64067d5f8e34b048af620
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-05-04 14:27:02 -0500

    admin template

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyA9p66rW4f4bVIqHxTURZG3wW8LeuuLgfg",
+    authDomain: "julio-melchor-portfolio.firebaseapp.com",
+    databaseURL: "https://julio-melchor-portfolio.firebaseio.com",
+    projectId: "julio-melchor-portfolio",
+    storageBucket: "julio-melchor-portfolio.appspot.com",
+    messagingSenderId: "751029913474",
+    appId: "1:751029913474:web:c1b8bd78377dd67d46f19b",
+    measurementId: "G-52GT299Z06"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyA9p66rW4f4bVIqHxTURZG3wW8LeuuLgfg",
+    authDomain: "julio-melchor-portfolio.firebaseapp.com",
+    databaseURL: "https://julio-melchor-portfolio.firebaseio.com",
+    projectId: "julio-melchor-portfolio",
+    storageBucket: "julio-melchor-portfolio.appspot.com",
+    messagingSenderId: "751029913474",
+    appId: "1:751029913474:web:c1b8bd78377dd67d46f19b",
+    measurementId: "G-52GT299Z06"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
